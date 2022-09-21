8d9cef8fcc4c4f7721432ee4f93fccfaa80222b1
ohsaraho <osg21001@byui.edu>
2022-09-21 00:47:17 -0600

    Data showing on local host

--- lesson1/app.js
+++ lesson1/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const professionalRoutes = require('./routes/professional');
-
+// const port =  process.env.PORT || 8080;
 const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello world!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example port is ${port}`)
+// })
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
